4e7c80b1fdbaab3b82120f20e1dc1db7b54bded9
Max Base <maxbasecode@gmail.com>
2022-02-05 00:14:27 +0330

    rename the repo

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright 2021 by the contributors <https://github.com/BaseMax/PolynomialAlgorithm/contributors>
+ *  Copyright 2021 by the contributors <https://github.com/BaseMax/PolynomialDivideCPP/contributors>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

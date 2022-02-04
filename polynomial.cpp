aeeaf8a9b83091e2171fe575f6578c0b384e7aa6
Max Base <MaxBaseCode@Gmail.Com>
2022-02-04 17:51:52 +0330

    C++: Improve and add comments

--- polynomial.cpp
+++ polynomial.cpp
@@ -11,8 +11,7 @@ struct Node {
 	struct Node* next;
 };
 
-void create_node(float x, int y,
-				struct Node** temp)
+void create_node(float x, int y, struct Node** temp)
 {
 	struct Node *r, *z;
 	z = *temp;
@@ -37,8 +36,7 @@ void create_node(float x, int y,
 		r->next = NULL;
 	}
 }
-void store_quotient(float mul_c, int diff,
-					struct Node* quo)
+void store_quotient(float mul_c, int diff, struct Node* quo)
 {
 	while (quo->next != NULL) {
 		quo = quo->next;
@@ -50,8 +48,7 @@ void store_quotient(float mul_c, int diff,
 	quo = quo->next;
 	quo->next = NULL;
 }
-void formNewPoly(int diff, float mul_c,
-				struct Node* poly)
+void formNewPoly(int diff, float mul_c, struct Node* poly)
 {
 	while (poly->next != NULL) {
 		poly->pow += diff;
@@ -59,14 +56,14 @@ void formNewPoly(int diff, float mul_c,
 		poly = poly->next;
 	}
 }
-void copyList(struct Node* r,
-			struct Node** copy)
+void copyList(struct Node* r, struct Node** copy)
 {
 	while (r != NULL) {
 
 		struct Node* z
 			= (struct Node*)malloc(
 				sizeof(struct Node));
+		// Store coefficient and power
 		z->coeff = r->coeff;
 		z->pow = r->pow;
 		z->next = NULL;
@@ -84,13 +81,10 @@ void copyList(struct Node* r,
 		r = r->next;
 	}
 }
-void polySub(struct Node* poly1,
-			struct Node* poly2,
-			struct Node* poly)
+void polySub(struct Node* poly1, struct Node* poly2, struct Node* poly)
 {
 	while (poly1->next && poly2->next) {
 		if (poly1->pow > poly2->pow) {
-
 			poly->pow = poly1->pow;
 			poly->coeff = poly1->coeff;
 			poly1 = poly1->next;
@@ -171,8 +165,7 @@ void show(struct Node* node)
 
 	cout << "\n";
 }
-void divide_poly(struct Node* poly1,
-				struct Node* poly2)
+void divide_poly(struct Node* poly1, struct Node* poly2)
 {
 	struct Node *rem = NULL, *quo = NULL;
 
@@ -183,7 +176,8 @@ void divide_poly(struct Node* poly1,
 	struct Node *q = NULL, *r = NULL;
 	copyList(poly1, &q);
 	copyList(poly2, &r);
-	while (q != NULL && (q->pow >= poly2->pow))
+	while (q != NULL
+		&& (q->pow >= poly2->pow)) {
 		int diff = q->pow - poly2->pow;
 
 		float mul_c = (q->coeff
@@ -202,21 +196,35 @@ void divide_poly(struct Node* poly1,
 		q = store;
 		free(q2);
 	}
+
+	// Print the quotient
 	cout << "Quotient: ";
 	show(quo);
+
+	// Print the remainder
 	cout << "Remainder: ";
 	rem = q;
 	show(rem);
 }
+
+// Driver Code
 int main()
 {
 	struct Node* poly1 = NULL;
 	struct Node *poly2 = NULL, *poly = NULL;
+
+	// Create 1st Polynomial (Dividend):
+	// 5x^2 + 4x^1 + 2
 	create_node(5.0, 2, &poly1);
 	create_node(4.0, 1, &poly1);
 	create_node(2.0, 0, &poly1);
+
+	// Create 2nd Polynomial (Divisor):
+	// 5x^1 + 5
 	create_node(5.0, 1, &poly2);
 	create_node(5.0, 0, &poly2);
+
+	// Function Call
 	divide_poly(poly1, poly2);
 
 	return 0;
